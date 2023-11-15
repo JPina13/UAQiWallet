f29e3cbc0ff466c02983326326a8db7d53b577e0
JPiña <jpina13@alumnos.uaq.mx>
2023-11-14 22:08:24 -0600

    new style and complements for the users information

--- app/src/main/java/com/example/uaqwallet/Register.java
+++ app/src/main/java/com/example/uaqwallet/Register.java
@@ -11,25 +11,34 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.ktx.Firebase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
     //Creamos las variables y los componentes que corresponden
     TextInputEditText editTextEmail, editTextPassword;
+    EditText nameUs, expUser, facultadUser, noTarjeta;
     Button buttonReg;
     FirebaseAuth mAuth;
     TextView textView;
+    private FirebaseFirestore mfirestore;
 
     @Override
     public void onStart() {
@@ -50,9 +59,15 @@ public class Register extends AppCompatActivity {
         //Instanciar nuestrar variables con nuestros componentes y sus ID's
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
+        mfirestore = FirebaseFirestore.getInstance();
+
         ProgressBar progressBar;
 
         editTextEmail = findViewById(R.id.emailRe);
+        nameUs = findViewById(R.id.nameUser);
+        expUser = findViewById(R.id.exped);
+        facultadUser = findViewById(R.id.facultadUser);
+        noTarjeta = findViewById(R.id.noTarjeta);
         editTextPassword = findViewById(R.id.passwordRe);
         buttonReg = findViewById(R.id.btn_aceptar);
         progressBar = findViewById(R.id.progressBarRe);
@@ -74,11 +89,15 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String email, password;
+                String email, password, nameU, expe, facu, noTargett;
 
                 //Optenemos los datos del usuario que ingreso en los editText
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
+                nameU = nameUs.getText().toString().trim();
+                expe = expUser.getText().toString().trim();
+                facu = facultadUser.getText().toString().trim();
+                noTargett = noTarjeta.getText().toString().trim();
 
                 //Evitamos que esten vacios los campos
                 if (TextUtils.isEmpty(email)){
@@ -89,6 +108,24 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this,"Ingresa una contraseña", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if (nameU.isEmpty() && expe.isEmpty() && facu.isEmpty() && noTargett.isEmpty()){
+                    Toast.makeText(Register.this,"Llene todos los datos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Map<String, Object> map = new HashMap<>();
+                map.put("nombre", nameU);
+                map.put("expediente", expe);
+                map.put("facultad", facu);
+                map.put("tarjeta", noTargett);
+
+                mfirestore.collection("Users").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(Register.this,"Datos agregados", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
 
                 //Mostramos la barra de progreso
                 progressBar.setVisibility(view.VISIBLE);
